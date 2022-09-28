1aaadefce454a0e69731150d493ceac8d631d4d1
gudongbin <rnehdqls112@gmail.com>
2022-09-28 14:59:30 +0900

    First hello.py

---
+++ hello.py
@@ -0,0 +1,13 @@
+avg = (5476) / 2
+print("Average:", avg)
+if avg >= 70:
+    print("You're a Pass!")
+else:
+    print("You're rejected!")
+
+print('당신의 이름은 무엇인가요?')
+name = input()
+print(name, '님 반가워요.')
+
+h = int(input('시간을 적으십시오'))
+print(h,'시간은', h*60,분입니다.)
